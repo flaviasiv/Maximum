862931004cd7e4f186da17e9a5d4a7bafdf12935
flavia sigoli <flaviasivm@gmail.com>
2024-03-07 16:23:49 -0300

    alteracoes1 97-03

--- js/javascript.js
+++ js/javascript.js
@@ -14,37 +14,37 @@ document.onclick = function(e){
     }
 }
 // scroll navbar
-const navbar = document.getElementById("homenav");
+// const navbar = document.getElementById("homenav");
 
-let lastScrollTop = 0;
-addEventListener("scroll", () => {
-    // Current scroll position
-    const scrollTop =
-        window.pageYOffset || document.documentElement.scrollTop;
+// let lastScrollTop = 0;
+// addEventListener("scroll", () => {
+//     // Current scroll position
+//     const scrollTop =
+//         window.pageYOffset || document.documentElement.scrollTop;
 
-    // check scroll direction
-    const distance = scrollTop - lastScrollTop;
-    const currentTop = parseInt(
-        getComputedStyle(navbar).top.split("px")
-    );
+//     // check scroll direction
+//     const distance = scrollTop - lastScrollTop;
+//     const currentTop = parseInt(
+//         getComputedStyle(navbar).top.split("px")
+//     );
 
-    // Clamp value between -80 and 0
-    let amount = Math.max(
-        Math.min(
-            currentTop +
-                (distance < 0
-                    ? Math.abs(distance) // scrolling up
-                    : -Math.abs(distance)// scrolling down
-                    ) * 40, // (1)
-            0
-        ),
-        -80
-    );
+//     // Clamp value between -80 and 0
+//     let amount = Math.max(
+//         Math.min(
+//             currentTop +
+//                 (distance < 0
+//                     ? Math.abs(distance) // scrolling up
+//                     : -Math.abs(distance)// scrolling down
+//                     ) * 40, // (1)
+//             0
+//         ),
+//         -80
+//     );
 
-    console.log(amount, currentTop, Math.abs(distance));
-    navbar.style.top = `${amount}px`;
-    lastScrollTop = scrollTop;
-});
+//     console.log(amount, currentTop, Math.abs(distance));
+//     navbar.style.top = `${amount}px`;
+//     lastScrollTop = scrollTop;
+// });
 
 // scroll behavior
 function reveal() {
@@ -84,61 +84,3 @@ window.addEventListener("scroll", reveal);
 // function closePopup(){
 //   popup.classList.remove("open-popup");
 // }
-
-
-//countUp about
-// let section_counter = document.querySelector('#section_counter');
-// let counters = document.querySelectorAll('.counter-item .counter');
-
-// Scroll Animation
-
-// let CounterObserver = new IntersectionObserver(
-//   (entries, observer) => {
-//     let [entry] = entries;
-//     if (!entry.isIntersecting) return;
-
-//     let speed = 500;
-//     counters.forEach((counter, index) => {
-//       function UpdateCounter() {
-//         const targetNumber = +counter.dataset.target;
-//         const initialNumber = +counter.innerText;
-//         const incPerCount = targetNumber / speed;
-//         if (initialNumber < targetNumber) {
-//           counter.innerText = Math.ceil(initialNumber + incPerCount);
-//           setTimeout(UpdateCounter, 10);
-//         }
-//       }
-//       UpdateCounter();
-
-//       if (counter.parentElement.style.animation) {
-//         counter.parentElement.style.animation = '';
-//       } else {
-//         counter.parentElement.style.animation = ` 0.3s ease forwards ${
-//           index / counters.length + 0.1
-//         }s`;
-//       }
-//     });
-//     observer.unobserve(section_counter);
-//   },
-//   {
-//     root: null,
-//     threshold: window.innerWidth > 768 ? 0.4 : 0.3,
-//   }
-// );
-
-// CounterObserver.observe(section_counter);
-
-//map google
-//let map;
-//
-//async function initMap() {
-//  //@ts-ignore
-//  const { Map } = await google.maps.importLibrary("map iframe");
-//
-//  map = new Map(document.getElementById("map"), {
-//    center: { lat: -34.397, lng: 150.644 },
-//    zoom: 8,
-//  });
-//}
-//
-//initMap();
